b9e82a47c3450a9a7c24e0f322f1e0f3f8c8070a
Natalie Dreher <n-dreher@hotmail.com>
2020-07-14 15:03:45 -0600

    Revert "touch ups"
    This reverts commit 11a3d8c63ef14aa0181040c4a0a8ff1d3adc8d05.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,24 +6,27 @@ import BGImage from "../../images/344_243_1558895986144635898004111912095(2).jpg
 
 
 function Main() {
-  return (
-    <Parallax bgImage={BGImage} bgImageSizes="100%"
-      renderLayer={percentage => (
-        <div>
-          <h1 style={{
-            position: "absolute",
-            left: "50%",
-            top: "50%",
-            transform: "translate(-50%,-50%)",
-            fontSize: (240 / (6 * percentage * percentage)) - 80,
-            zIndex: 100
-          }}>Natalie Dreher</h1>
+    return(
+        <Parallax bgImage={BGImage} bgImageSizes="100%" 
+        renderLayer={percentage => (
+          <div>
+            <h1 style={{ 
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%,-50%)",
+                fontSize: (240 / (6 * percentage * percentage)) - 80,
+                zIndex: 100
+            }}>Natalie Dreher</h1>
+            
+          </div>
+        )}
+      >
+        <div style={{ height: "100vh", position: "relative" }}>
+ 
         </div>
-      )}
-    >
-      <div style={{ height: "100vh", position: "relative" }} />
-    </Parallax>
-  );
+        </Parallax>
+    );
 
 }
 

00c2948b9259689f92c6ae9407be5bd2cb7e6c23
Natalie Dreher <n-dreher@hotmail.com>
2020-07-14 15:25:14 -0600

    Updates

---
+++ precache-manifest.abafbab0d2008020f39350cb260bf435.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4dcde0190e765e3097e2b3f398caa74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "682920fca35fda0310f6",
+    "url": "/static/css/2.26655abe.chunk.css"
+  },
+  {
+    "revision": "46102a96d2668f54f674",
+    "url": "/static/css/main.a4a8a432.chunk.css"
+  },
+  {
+    "revision": "682920fca35fda0310f6",
+    "url": "/static/js/2.d9c17fc7.chunk.js"
+  },
+  {
+    "revision": "ae811a3afae7f14104d3c9a77f6764e6",
+    "url": "/static/js/2.d9c17fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46102a96d2668f54f674",
+    "url": "/static/js/main.4e8d0da5.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8b09c53eff9f3042f4971f5e9090bc58",
+    "url": "/static/media/344_243_1558895986144635898004111912095(2).8b09c53e.jpg"
+  },
+  {
+    "revision": "d616c1cdee2b60b71ce16cb590e20a3e",
+    "url": "/static/media/Amtrak_Cascades_cars_1.d616c1cd.jpg"
+  },
+  {
+    "revision": "087292cd6aad2ee0fef6541d038b112e",
+    "url": "/static/media/Boostrap-Logo.087292cd.svg"
+  },
+  {
+    "revision": "0d61aec517f7ce771aa38aea005bd81b",
+    "url": "/static/media/CSS-Logo.0d61aec5.svg"
+  },
+  {
+    "revision": "1c43f1a6bb5f1b54bd0fdffbd45f9892",
+    "url": "/static/media/ClickHole.1c43f1a6.jpg"
+  },
+  {
+    "revision": "ec7a8fdf1f15cc203946f668537c967a",
+    "url": "/static/media/DA342876-1.ec7a8fdf.jpg"
+  },
+  {
+    "revision": "84c147064df993fd69d13c6531347d43",
+    "url": "/static/media/Giphy-Logo.84c14706.png"
+  },
+  {
+    "revision": "e9bd75dc885e3c2bcffd0a4467b2e027",
+    "url": "/static/media/HTML-Logo.e9bd75dc.svg"
+  },
+  {
+    "revision": "b012529ab0ac6954f6ae34038bb9ebf6",
+    "url": "/static/media/JS-Logo.b012529a.svg"
+  },
+  {
+    "revision": "1a328de2c7a11c2930c547b2cc5aed9c",
+    "url": "/static/media/MongoDB-Logo.1a328de2.svg"
+  },
+  {
+    "revision": "e8460edf53b44c20bfdb8c16bedf2e59",
+    "url": "/static/media/MySQL-Logo.e8460edf.svg"
+  },
+  {
+    "revision": "8b74423e7d179b15f98fb0492d14bc64",
+    "url": "/static/media/NJlh-4sg_400x400.8b74423e.jpg"
+  },
+  {
+    "revision": "89ccb09060a7adf788ea2340c54d6d78",
+    "url": "/static/media/Node-Logo.89ccb090.svg"
+  },
+  {
+    "revision": "5f0931d4e6a64cde862793480acd17cd",
+    "url": "/static/media/React-Logo.5f0931d4.svg"
+  },
+  {
+    "revision": "a23d5c18d58d40beb63b73d2bd146f8b",
+    "url": "/static/media/Steven_Shield_WD.a23d5c18.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "d8a18a804f5186f9629e5596c8bd0bc5",
+    "url": "/static/media/dollar-sign-white.d8a18a80.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/static/media/nodejslogo.6dcf6999.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "f86d1ff57430f1ace9e4abc3a02ecb3f",
+    "url": "/static/media/pigeon_key2_400.f86d1ff5.jpg"
+  }
+]);
